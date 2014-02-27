f7c713c415f326e3210e49ecc3402e0a7ef9e79b
DanCarlyon <dannycar@hotmail.co.uk>
2014-02-27 16:25:15 +0000

    First theme Commit

---
+++ functions.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * Twenty Twelve functions and definitions
+ *
+ * Sets up the theme and provides some helper functions, which are used
+ * in the theme as custom template tags. Others are attached to action and
+ * filter hooks in WordPress to change core functionality.
+ *
+ * When using a child theme (see http://codex.wordpress.org/Theme_Development and
+ * http://codex.wordpress.org/Child_Themes), you can override certain functions
+ * (those wrapped in a function_exists() call) by defining them first in your child theme's
+ * functions.php file. The child theme's functions.php file is included before the parent
+ * theme's file, so the child theme functions would be used.
+ *
+ * Functions that are not pluggable (not wrapped in function_exists()) are instead attached
+ * to a filter or action hook.
+ *
+ * For more information on hooks, actions, and filters, @link http://codex.wordpress.org/Plugin_API
+ *
+ * @package WordPress
+ * @subpackage Rust_Bucket
+ * @since RustBucket 2014
+ */
+
+
+/**
+ * Register sidebars.
+ *
+ * Registers our main widget area and the front page widget areas.
+ *
+ * @since Twenty Twelve 1.0
+ */
+function rustbucket_widgets_init() {
+	register_sidebar( array(
+		'name' => __( 'Main Sidebar', 'rustbucket' ),
+		'id' => 'sidebar-1',
+		'description' => __( 'Appears on posts and pages except the optional Front Page template, which has its own widgets', 'rustbucket' ),
+		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		'after_widget' => '</aside>',
+		'before_title' => '<h3 class="widget-title">',
+		'after_title' => '</h3>',
+	) );
+
+	register_sidebar( array(
+		'name' => __( 'First Front Page Widget Area', 'rustbucket' ),
+		'id' => 'sidebar-2',
+		'description' => __( 'Appears when using the optional Front Page template with a page set as Static Front Page', 'rustbucket' ),
+		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		'after_widget' => '</aside>',
+		'before_title' => '<h3 class="widget-title">',
+		'after_title' => '</h3>',
+	) );
+
+	register_sidebar( array(
+		'name' => __( 'Second Front Page Widget Area', 'rustbucket' ),
+		'id' => 'sidebar-3',
+		'description' => __( 'Appears when using the optional Front Page template with a page set as Static Front Page', 'rustbucket' ),
+		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		'after_widget' => '</aside>',
+		'before_title' => '<h3 class="widget-title">',
+		'after_title' => '</h3>',
+	) );
+}
+add_action( 'widgets_init', 'rustbucket_widgets_init' );
---
+++ header.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * The Header template for our theme
+ *
+ * Displays all of the <head> section and everything up till <div id="main">
+ *
+ * @package WordPress
+ * @subpackage rust_bucket
+ * @since RustBucket 2014
+ */
+?><!DOCTYPE html>
+<!--[if IE 7]>
+<html class="ie ie7" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if IE 8]>
+<html class="ie ie8" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if !(IE 7) | !(IE 8)  ]><!-->
+<html <?php language_attributes(); ?>>
+<!--<![endif]-->
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>" />
+<meta name="viewport" content="width=device-width" />
+<title><?php wp_title( '|', true, 'right' ); ?></title>
+<link rel="profile" href="http://gmpg.org/xfn/11" />
+<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+
+<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css" />
+<link href="<?php echo get_template_directory_uri(); ?>/css/jquery.bxslider.css" rel="stylesheet" />
+
+<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+<script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/jquery.bxslider.min.js"></script>
+
+<script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/scripts.js"></script>
+
+
+<script>
+$(function() {
+
+	$('.bxslider').bxSlider({
+		infiniteLoop:true,
+		captions:true,
+		responsive:true,
+		useCSS:true,
+		touchEnabled:true,
+		controls:false,
+		auto:true
+	});
+});
+</script>
+
+
+<?php // Loads HTML5 JavaScript file to add support for HTML5 elements in older IE versions. ?>
+<!--[if lt IE 9]>
+<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
+<![endif]-->
+<?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?> style="background:url(<?php echo get_template_directory_uri(); ?>/assets/rustbucket_background.png) no-repeat;background-attachment:fixed;">
+
+<header id="masthead" class="site-header" role="banner">
+    <hgroup>
+    	<div class="color">&nbsp;</div>
+    	<div class="logo-menu-wrapper">
+			<div class="middle">
+		        <span class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></span>
+		        <span class="site-description"><?php bloginfo( 'description' ); ?></span>
+	        </div>
+		</div>
+    </hgroup>
+
+    <!--<nav id="site-navigation" class="main-navigation" role="navigation">
+        <h3 class="menu-toggle"><?php _e( 'Menu', 'rustbucket' ); ?></h3>
+        <a class="assistive-text" href="#content" title="<?php esc_attr_e( 'Skip to content', 'rustbucket' ); ?>"><?php _e( 'Skip to content', 'rustbucket' ); ?></a>
+        <?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'nav-menu' ) ); ?>
+    </nav> -->
+
+    <?php if ( get_header_image() ) : ?>
+        <a href="<?php echo esc_url( home_url( '/' ) ); ?>"><img src="<?php header_image(); ?>" class="header-image" width="<?php echo get_custom_header()->width; ?>" height="<?php echo get_custom_header()->height; ?>" alt="" /></a>
+    <?php endif; ?>
+</header><!-- #masthead -->
+
+<div id="page" class="hfeed site" style="position:relative;">
+	
