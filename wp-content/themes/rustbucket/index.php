1ca24e40f5835b605a08585c6c536bf9c81078f0
DanCarlyon <dannycar@hotmail.co.uk>
2014-03-02 11:26:00 +0000

    Forums are now working, just need styling

--- wp-content/themes/rustbucket/index.php
+++ wp-content/themes/rustbucket/index.php
@@ -38,8 +38,21 @@ get_header(); ?>
 					 * use this in a child theme, then include a file called called content-___.php
 					 * (where ___ is the post format) and that will be used instead.
 					 */
-					get_template_part( 'content', get_post_format() );
+                        ?>
+							<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+                            <?php
+                                // Page thumbnail and title.
+                                the_title( '<header class="entry-header"><h1 class="entry-title">', '</h1></header><!-- .entry-header -->' );
+                            ?>
+                        
+                            <div class="entry-content">
+                                <?php
+                                    the_content();
+                                ?>
+                            </div><!-- .entry-content -->
+                        </article><!-- #post-## -->
 
+            <?php
 				endwhile;
 				// Previous/next post navigation.
 				twentyfourteen_paging_nav();
